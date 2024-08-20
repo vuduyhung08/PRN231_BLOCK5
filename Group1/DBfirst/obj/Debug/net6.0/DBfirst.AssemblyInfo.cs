fe493c1f12fda5c0c915e2cfd4617fbba6af3330
TLupus131 <hahacondien@gmail.com>
2024-08-20 15:13:54 +0700

    update Email Service

--- Group1/DBfirst/obj/Debug/net6.0/DBfirst.AssemblyInfo.cs
+++ Group1/DBfirst/obj/Debug/net6.0/DBfirst.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBfirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77044637f88ee6a361c908208e0d3bec1c4bf4c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c394c3c6150aac4e6ce85ee67026edf8cd6631b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBfirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBfirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
