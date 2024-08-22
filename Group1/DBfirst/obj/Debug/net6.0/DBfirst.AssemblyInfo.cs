7e0f8874abd5225ad9a201ad297f17935cfbc742
toanvk <142169369+toanvk@users.noreply.github.com>
2024-08-22 08:00:35 +0700

    changes

--- Group1/DBfirst/obj/Debug/net6.0/DBfirst.AssemblyInfo.cs
+++ Group1/DBfirst/obj/Debug/net6.0/DBfirst.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBfirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0618064f0106b7401b066d701e85b87900a680e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8732e945ce0ac063b48c73d4da0a54e4fbf7cc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBfirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBfirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
